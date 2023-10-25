795c60c468c7612d7262d9ed6a6bd98785f8fcb0
d33bs <dave.bunten@cuanschutz.edu>
2023-10-25 16:05:14 -0600

    move to __main__ def and hashbang syntax

--- notebooks/examples/cytotable_convert_nf1.py
+++ notebooks/examples/cytotable_convert_nf1.py
@@ -1,24 +1,31 @@
+#!/usr/bin/env python
+
 """
-Demonstrating CytoTable capabilities with input datasets
+Demonstrating CytoTable capabilities with input datasets.
+Note: intended to be used for profiling via memray.
 """
 import cytotable
-
 import pathlib
 import sys
 
-# take an input from sys argsv
-input_file = sys.argv[1]
 
-# Get the path of the current module an use it as a subdir
-dest_path = f"{pathlib.Path(__file__).parent.resolve()}/{pathlib.Path(input_file).name}"
+def main():
+    input_file = sys.argv[1]
+    dest_path = (
+        f"{pathlib.Path(__file__).parent.resolve()}/{pathlib.Path(input_file).name}"
+    )
 
-result = cytotable.convert(
-    source_path=input_file,
-    dest_path=dest_path,
-    dest_datatype="parquet",
-    source_datatype="sqlite",
-    preset="cellprofiler_sqlite_pycytominer",
-    chunk_size=200000,
-)
+    result = cytotable.convert(
+        source_path=input_file,
+        dest_path=dest_path,
+        dest_datatype="parquet",
+        source_datatype="sqlite",
+        preset="cellprofiler_sqlite_pycytominer",
+        chunk_size=200000,
+    )
 
-pathlib.Path(dest_path).unlink()
+    pathlib.Path(dest_path).unlink()
+
+
+if __name__ == "__main__":
+    main()
--- notebooks/examples/pycytominer_merge_nf1.py
+++ notebooks/examples/pycytominer_merge_nf1.py
@@ -1,38 +1,45 @@
+#!/usr/bin/env python
+
 """
-Demonstrating CytoTable capabilities with input datasets
+Demonstrating CytoTable capabilities with input datasets.
+Note: intended to be used for profiling via memray.
 """
 from pycytominer.cyto_utils.cells import SingleCells
-
 import pathlib
 import sys
 
-# take an input from sys argsv
-input_file = sys.argv[1]
 
-# Get the path of the current module an use it as a subdir
-dest_path = f"{pathlib.Path(__file__).parent.resolve()}/{pathlib.Path(input_file).name}"
+def main():
+    input_file = sys.argv[1]
+    dest_path = (
+        f"{pathlib.Path(__file__).parent.resolve()}/{pathlib.Path(input_file).name}"
+    )
 
-result = SingleCells(
-    sql_file=f"sqlite:///{input_file}",
-    compartments=["Per_Cells", "Per_Cytoplasm", "Per_Nuclei"],
-    compartment_linking_cols={
-        "Per_Cytoplasm": {
-            "Per_Cells": "Cytoplasm_Parent_Cells",
-            "Per_Nuclei": "Cytoplasm_Parent_Nuclei",
+    result = SingleCells(
+        sql_file=f"sqlite:///{input_file}",
+        compartments=["Per_Cells", "Per_Cytoplasm", "Per_Nuclei"],
+        compartment_linking_cols={
+            "Per_Cytoplasm": {
+                "Per_Cells": "Cytoplasm_Parent_Cells",
+                "Per_Nuclei": "Cytoplasm_Parent_Nuclei",
+            },
+            "Per_Cells": {"Per_Cytoplasm": "Cells_Number_Object_Number"},
+            "Per_Nuclei": {"Per_Cytoplasm": "Nuclei_Number_Object_Number"},
         },
-        "Per_Cells": {"Per_Cytoplasm": "Cells_Number_Object_Number"},
-        "Per_Nuclei": {"Per_Cytoplasm": "Nuclei_Number_Object_Number"},
-    },
-    image_table_name="Per_Image",
-    strata=["Image_Metadata_Well", "Image_Metadata_Plate"],
-    merge_cols=["ImageNumber"],
-    image_cols="ImageNumber",
-    load_image_data=True,
-    # perform merge_single_cells without annotation
-    # and receive parquet filepath
-).merge_single_cells(
-    sc_output_file=dest_path,
-    output_type="parquet",
-)
+        image_table_name="Per_Image",
+        strata=["Image_Metadata_Well", "Image_Metadata_Plate"],
+        merge_cols=["ImageNumber"],
+        image_cols="ImageNumber",
+        load_image_data=True,
+        # perform merge_single_cells without annotation
+        # and receive parquet filepath
+    ).merge_single_cells(
+        sc_output_file=dest_path,
+        output_type="parquet",
+    )
 
-pathlib.Path(dest_path).unlink()
+    pathlib.Path(dest_path).unlink()
+
+
+if __name__ == "__main__":
+    main()
